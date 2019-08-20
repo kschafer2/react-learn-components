5b7132c50d926de698d2b2f1f1bef8d9c3bd3ff3
Kyle Schafer <kschay@protonmail.com>
2019-08-20 16:55:19 -0500

    Closes #6 - Wrapped each CommentDetail in an ApprovalCard

--- src/index.js
+++ src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        avatar={Faker.image.avatar()}
-        content="Nice blog post!"
-        timeAgo="Today at 6:00PM"
-      />
-      <CommentDetail
-        author="Alex"
-        avatar={Faker.image.avatar()}
-        content="Thanks for the info!"
-        timeAgo="Today at 5:13PM"
-      />
-      <CommentDetail
-        author="Jane"
-        avatar={Faker.image.avatar()}
-        content="You're the best!"
-        timeAgo="Yesterday at 6:47PM"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          avatar={Faker.image.avatar()}
+          content="Nice blog post!"
+          timeAgo="Today at 6:00PM"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          avatar={Faker.image.avatar()}
+          content="Thanks for the info!"
+          timeAgo="Today at 5:13PM"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          avatar={Faker.image.avatar()}
+          content="You're the best!"
+          timeAgo="Yesterday at 6:47PM"
+        />
+      </ApprovalCard>
     </div>
   );
 };
