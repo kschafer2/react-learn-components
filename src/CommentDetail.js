9bc9780c1b0aaddd70e48ebe18a4a9c6928d8a53
Kyle Schafer <kschay@protonmail.com>
2019-08-20 16:33:58 -0500

    Closes #4 - Created 3 Different Comments with React Props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import Faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = props => {
+  console.log(props);
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={Faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
-        <a href="/" className="author">
-          Sam
-        </a>
-        <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
-        </div>
-        <div className="text">Nice blog post!</div>
+        <a href="/" className="author">{props.author}</a>
+      <div className="metadata">
+        <span className="date">{props.timeAgo}</span>
+      </div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -7,7 +7,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        avatar={Faker.image.avatar()}
+        content="Nice blog post!"
+        timeAgo="Today at 6:00PM"
+      />
+      <CommentDetail
+        author="Alex"
+        avatar={Faker.image.avatar()}
+        content="Thanks for the info!"
+        timeAgo="Today at 5:13PM"
+      />
+      <CommentDetail
+        author="Jane"
+        avatar={Faker.image.avatar()}
+        content="You're the best!"
+        timeAgo="Yesterday at 6:47PM"
+      />
     </div>
   );
 };
